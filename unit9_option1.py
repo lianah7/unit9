2d7569711ddf264c2c587d6dc81c8f0d50e962f0
liana <hilllia@ssfs.org>
2019-12-05 12:50:40 -0500

    finished flowchart and started unit 9 project option 1

---
+++ unit9_option1.py
@@ -0,0 +1,12 @@
+# created by Liana Hill
+# last modified on December 5, 2019
+# unit 9 option one - this program plays a game of Compare (a small game of War) between the user and dealer
+
+import card
+import deck
+
+user_cards =[]
+dealer_cards = []
+shuffle = shuffle.deck()
+
+def deal_card():
