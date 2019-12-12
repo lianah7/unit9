ec6fff1052390bbfba077f3c3a7669d8ac52c748
liana <hilllia@ssfs.org>
2019-12-12 09:18:31 -0500

    finished unit 9 project

--- unit9_option1.py
+++ unit9_option1.py
@@ -2,11 +2,15 @@
 # last modified on December 5, 2019
 # unit 9 option one - this program plays a game of Compare (a small game of War) between the user and dealer
 
-import card
 import deck
 
 
 def deal_cards(my_deck):
+    """
+    This function deals five cards to the user and dealer.
+    :param my_deck: the deck of 52 cards
+    :return: returns user's cards
+    """
     user_cards = []
     for x in range(5):
         user_card = my_deck.deal()
@@ -15,6 +19,12 @@ def deal_cards(my_deck):
 
 
 def compare_cards(user_card, dealer_card):
+    """
+    This function compares the cards of the dealer and user based on the rank
+    :param user_card: the user's five cards
+    :param dealer_card: the dealer's five cards
+    :return: true if the rank of the user's card rank is higher and false if the dealer's card rank is higher
+    """
     if user_card > dealer_card:
         print("The user wins!")
         return True
@@ -38,8 +48,11 @@ def main():
         else:
             dealer_score += 1
         print("")
-        print("The user's final score is", user_score, "and the dealer's final score is", dealer_score)
-
+    print("The user's final score is", user_score, "and the dealer's final score is", dealer_score)
+    if user_score > dealer_score:
+        print("The user won the game!")
+    else:
+        print("The dealer won the game!")
 
 
 main()
